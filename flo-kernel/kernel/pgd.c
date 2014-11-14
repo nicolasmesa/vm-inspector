bf1dddfcb961a0fab5664f7a87e8cf2a5d8d80b2
Nicolas Mesa <nm2805@columbia.edu>
2014-11-13 22:31:44 -0500

    Adding functionality to syscall

--- flo-kernel/kernel/pgd.c
+++ flo-kernel/kernel/pgd.c
@@ -1,13 +1,56 @@
-#include<linux/unistd.h>
-#include<linux/linkage.h>
-#include<linux/kernel.h>
-#include<linux/syscalls.h>
+#include <linux/unistd.h>
+#include <linux/linkage.h>
+#include <linux/kernel.h>
+#include <linux/syscalls.h>
+#include <linux/sched.h>
+#include <linux/mm.h>
 
 SYSCALL_DEFINE3(expose_page_table, pid_t, pid, unsigned long, fake_pgd, unsigned long, addr)
 {
-	printk(KERN_WARNING "expose_page_table called\n");
+	struct mm_struct *mm, *curr_mm;
+	struct vm_area_struct *mmap, *vma;
+	struct task_struct *p;
+
+
+	if (pid == -1)
+		p = current;
+	else 
+		p =   find_task_by_vpid(pid);
+
+	if (p == NULL)
+		return -EINVAL;
+
+	if (fake_pgd == 0)
+		return -EINVAL;
+
+	if (addr == 0)
+		return -EINVAL;
+
+	if (p->mm == NULL)
+		return -EINVAL;
+
+	curr_mm = curr->mm;
+
+	mm = p->mm;
+	mmap = mm->mmap;
+
+	vma = find_vma(curr_mm, addr);
+
+	/* Shouldn't happen */
+	if (vma == NULL)
+		return -EINVAL;
+
+	/* Don't know what to do in this case */
+	if (vma->vm_start > addr || vma->vm_end < addr) {
+		printk("Returned VMA that doesn't contain address: Pid: %d\t\tStart address: %lu\t\tEnd address: %lu\t\tAddress: %lu\n", p->pid, vma->vm_start, vma->vm_end, addr);
+		return -EINVAL;
+	}
+	
+
+	
+
+	printk("Pid: %d\t\tStart address: %lu\t\tEnd address: %lu\t\tAddress: %lu\n", p->pid, vma->vm_start, vma->vm_end, addr);
 
 	return 0;
-
 }
 
--- vm_inspector/vm_inspector.c
+++ vm_inspector/vm_inspector.c
@@ -1,9 +1,40 @@
 #include <stdio.h>
 #include <sys/syscall.h>
 #include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/mman.h>
+
+
+int expose_page_table(pid_t pid, unsigned long fake_pgd,
+					unsigned long addr)
+{
+	return syscall(378, pid, fake_pgd, addr);
+}
+
 int main(int argc, char **argv)
 {
+	int pid, ret;
+	void *address, *fake_pgd_addr;
+	long addr, fake_pgd;
 
-	syscall(378, 0, 0, 0);
+	if (argc > 1)
+		pid = atoi(argv[1]);
+	else
+		pid = -1;
+
+
+	address = mmap(0, 4096, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
+	addr = (long) address;
+
+	fake_pgd_addr = mmap(0, 4096, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);;
+	fake_pgd = (long) fake_pgd_addr;
+
+	ret = expose_page_table(pid, fake_pgd, addr);
+
+	if (ret < 0)
+		printf("Error: %s\n", strerror(errno));
+
 	return 0;
 }
